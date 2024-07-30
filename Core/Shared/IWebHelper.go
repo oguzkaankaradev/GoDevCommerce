c7b7e85f94380c47b55ae289ed8aa30104ad5824
oguzkaankaradev <135004675+oguzkaankaradev@users.noreply.github.com>
2024-07-30 15:40:26 +0800

    TestMessahe

---
+++ Core/Shared/IWebHelper.go
@@ -0,0 +1,5 @@
+package shared
+
+type IWebHelper interface {
+	GetCurrentIpAddress() string
+}
--- Core/Shared/WebHelper.go
+++ Core/Shared/WebHelper.go
@@ -10,6 +10,12 @@ type HttpContextAccessor struct {
 	HttpContext *http.Request
 }
 
+func isIPAddressSet(address net.IP) bool {
+
+	ipv6Loopback := net.IPv6loopback
+	return address != nil && !address.Equal(ipv6Loopback)
+}
+
 func (httpContextAccessor *HttpContextAccessor) IsRequestAvailable() bool {
 
 	if httpContextAccessor.HttpContext == nil {
@@ -29,8 +35,50 @@ func (httpContextAccessor *HttpContextAccessor) IsRequestAvailable() bool {
 	return true
 }
 
-func isIPAddressSet(address net.IP) bool {
+func IsRequestAvailable(httpContextAccessor *HttpContextAccessor) bool {
+	if httpContextAccessor.HttpContext == nil {
+		return false
+	}
 
-	ipv6Loopback := net.IPv6loopback
-	return address != nil && !address.Equal(ipv6Loopback)
+	if httpContextAccessor == nil {
+		return false
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Recovered from panic:", r)
+		}
+	}()
+
+	return true
+}
+
+// GetCurrentIpAddress
+func GetCurrentIpAddress(httpContextAccessor *HttpContextAccessor) string {
+	if !IsRequestAvailable(httpContextAccessor) {
+		return ""
+	}
+
+	remoteIp := httpContextAccessor.HttpContext.RemoteAddr
+	ip, _, err := net.SplitHostPort(remoteIp)
+	if err != nil {
+		return ""
+	}
+
+	parsedIP := net.ParseIP(ip)
+	if parsedIP == nil {
+		return ""
+	}
+
+	// IPv6 Loopback control
+	if parsedIP.Equal(net.IPv6loopback) {
+		return net.IPv4(127, 0, 0, 1).String()
+	}
+
+	// IPv4 convert and string response
+	if ipv4 := parsedIP.To4(); ipv4 != nil {
+		return ipv4.String()
+	}
+
+	return parsedIP.String()
 }
